9b5a45fef5dc5b98247da347beaab413b4d05b57
Lovish kumar prajapati <prajapatilovish@gmail.com>
2020-02-18 12:55:16 +0530

    time added

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("feb 27, 2020 10:00:00").getTime();
+var countDownDate = new Date("feb 27, 2020 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -25,6 +25,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "TODAY";
   }
 }, 1000);
